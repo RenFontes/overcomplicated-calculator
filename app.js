2f44cbe179bd3a28f8afad7a674ae90f6fad5711
Renato Fontes <renatofontes@gmail.com>
2021-08-31 21:30:44 -0700

    Updated configuration files

--- app.js
+++ app.js
@@ -23,14 +23,17 @@ const sequelize = new Sequelize({
     }
 });
 
-(async () => {
+var promise = (async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.log(`Connection error ${error}`);
+        process.exit(1)
     }
 })();
+
+console.log(promise.err)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
